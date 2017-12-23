9ec3fe9879a027864e96aef883b373af3c614bff
Robert G. Raleigh <robert@helloworlddevs.com>
2017-12-22 21:34:42 -0800

    updated brunch-config.js, added skeleton partials

--- brunch-config.js
+++ brunch-config.js
@@ -1,4 +1,47 @@
 // See http://brunch.io for documentation.
+/* jshint esversion: 6 */
+(function () {
+  "use strict";
+  const config = {
+    files: {
+      javascripts: {
+        joinTo: {
+          "vendor.js": /^vendor/,
+          "app.js": /^app/
+        },
+        order: {
+          before: ["vendor/jquery.min.js", "vendor/foundation.min.js"]
+        }
+      },
+      stylesheets: {
+        joinTo: "app.css"
+      }
+    },
+    plugins: {
+      babel: { presets: ["es2015", "pre"] },
+      sass: {
+        options: {
+          includePaths: [
+            "node_modules/foundation-sites/scss",
+            "node_modules/motion-ui/src"
+          ]
+        }
+      },
+      paths: {
+        watched: ["app", "test", "vendor"]
+      },
+      npm: {
+        globals: {
+          $: "jquery",
+          jQuery: "jquery"
+        }
+      }
+    }
+  };
+  module.exports = config;
+}());
+
+
 exports.files = {
   javascripts: {
     joinTo: {
